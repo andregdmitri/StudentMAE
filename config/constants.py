5abc31b2c8904c3be5be8e5e2addf646a7db1204
andregdmitri <andregdmitri@gmail.com>
2025-11-09 23:48:01 -0300

    Change constants and example

--- config/constants.py
+++ config/constants.py
@@ -10,7 +10,7 @@ LEARNING_RATE = float(os.getenv('LEARNING_RATE', 0.001))
 BATCH_SIZE = int(os.getenv('BATCH_SIZE', 32))
 EPOCHS = int(os.getenv('EPOCHS', 50))
 INPUT_DIM = int(os.getenv('INPUT_DIM', 784))
-NUM_CLASSES = int(os.getenv('NUM_CLASSES', 10))
+NUM_CLASSES = int(os.getenv('NUM_CLASSES', 5))
 
 # Other settings
 DEVICE = os.getenv('DEVICE', 'cuda' if torch.cuda.is_available() else 'cpu')
